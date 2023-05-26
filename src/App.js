aec6b608fb0ca1425b3c68aaca11819f2e23ccee
Yoandri Garcia Palma <yoandrigp1992@gmail.com>
2023-05-25 20:19:10 -0400

    changes made from theia lab ejios-react-budget-redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+
+    
 
 
 import { AppProvider } from './context/AppContext';
@@ -15,19 +21,55 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */
+                            // Budget component 
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
+                        }     
 
-                        {/* Add Remaining component here under */}        
+                        {/* Add Remaining component here under */
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>                        
+                        }        
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {/* Add ExpenseTotal component here under */
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
+                        }    
+
+                        <h3 className='mt-3'>Allocation</h3>    
                        
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                        {/* Add ExpenseList component here under */
+                            //ExpenseList component
+                            <div className='row '>
+                                <div className='col-sm'>
+                                    <ExpenseList />
+                                </div>
+                            </div>
+                            
+                        }         
                 </div>
+                        {/* Add ExpenseItem component here under */
+                            //ExpenseItem component
+                            
+                        }  
+
+                        <h3 className='mt-3'>Change allocation</h3>
+                        {/* Add AllocationForm component here under */
+                            //AllocationForm component
+                            <div className='row mt-3'>
+                                <div className='col-sm'>
+                                    <AllocationForm/>
+                                </div>
+                            </div>
+                        }        
+
+                
             </div>
         </AppProvider>
     );
